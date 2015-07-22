7cd44aedd295a849331111ad4dab381a5d19cf87
Craig <craigp@travelplaces.co.uk>
2015-07-22 15:25:22 +0100

    Added Logging

--- src/main/java/uk/co/parknet/email/EmailSender.java
+++ src/main/java/uk/co/parknet/email/EmailSender.java
@@ -1,8 +1,12 @@
 package uk.co.parknet.email;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
 
 /**
  * 
@@ -19,7 +23,22 @@ public class EmailSender extends EmailJsonSender implements Processor
 	{
 		String jsonMessage = arg0.getIn().getBody(String.class);
 		setServer((String) arg0.getProperty("emailServerAddress"));
-		createEmail(jsonMessage);
-		sendEmail();
+		try
+		{
+			createEmail(jsonMessage);
+			sendEmail();
+		}
+		catch(AddressException e)
+		{
+			logger.error(e.getMessage());
+		}
+		catch(MessagingException e)
+		{
+			logger.error(e.getMessage());
+		}
+		catch(JSONException e)
+		{
+			logger.error(e.getMessage());
+		}
 	}
 }
